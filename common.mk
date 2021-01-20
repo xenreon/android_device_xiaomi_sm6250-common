aa369f5ede2a4ee4e45898087b6c2d03abc601a3
xenreon <main.xenreon@gmail.com>
2021-01-20 13:09:39 +0530

    sm6250-common: add android.hardware.graphics.allocator

--- common.mk
+++ common.mk
@@ -145,6 +145,10 @@ PRODUCT_PACKAGES += \
     memtrack.atoll
 
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.allocator@3.0-impl \
+    android.hardware.graphics.allocator@4.0-impl \
     android.hardware.graphics.composer@2.4-impl \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
