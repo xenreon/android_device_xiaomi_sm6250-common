31ebadd43e5803f31a7a6815d8a4133d4d093e83
xenreon <main.xenreon@gmail.com>
2021-01-20 13:43:26 +0530

    sm6250-common: add android.hardware.graphics.composer@2.1

--- common.mk
+++ common.mk
@@ -149,6 +149,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.allocator@3.0-impl \
     android.hardware.graphics.allocator@4.0-impl \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.composer@2.4-impl \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
